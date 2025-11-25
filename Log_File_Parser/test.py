ba1003e2deb095bd4e616f219f8783d36869d088
Shamanth Krishna <shamanthkrishna0@gmail.com>
2025-11-25 13:39:28 +0530

    Commented out test.py

--- Log_File_Parser/test.py
+++ Log_File_Parser/test.py
@@ -1,5 +1,5 @@
-a = "2015-10-19 14:21:43,552 INFO [main] org.apache.hadoop.metrics2.impl.MetricsConfig: loaded properties from hadoop-metrics2.properties"
-timestamp = a.split()[0:2]
-msg = a.split("] ")[1]
-print(" ".join(timestamp))
-print(msg)
+# a = "2015-10-19 14:21:43,552 INFO [main] org.apache.hadoop.metrics2.impl.MetricsConfig: loaded properties from hadoop-metrics2.properties"
+# timestamp = a.split()[0:2]
+# msg = a.split("] ")[1]
+# print(" ".join(timestamp))
+# print(msg)
